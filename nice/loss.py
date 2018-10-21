99278b6414c3d59545ae8fc669ad944d6beed30c
Paul Tang <paulswtang@gmail.com>
2018-10-21 16:26:09 -0400

    make logistic prior loss more stable with softplus

--- nice/loss.py
+++ nice/loss.py
@@ -3,6 +3,7 @@ Implementation of NICE log-likelihood loss.
 """
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import numpy as np
 
 # ===== ===== Loss Function Implementations ===== =====
@@ -12,49 +13,61 @@ factorizes, i.e. the output is (y1,y2,...,yK) ~ p(Y) s.t. p(Y) = p_1(Y1) * p_2(Y
 with each individual component's prior distribution coming from a standardized family of
 distributions, i.e. p_i == Gaussian(mu,sigma) for all i in 1..K, or p_i == Logistic(mu,scale).
 """
-def gaussian_nice_loglkhd(h, diag):
+def gaussian_nice_loglkhd(h):
     """
     Definition of log-likelihood function with a Gaussian prior, as in the paper.
     
     Args:
     * h: float tensor of shape (N,D). First dimension is batch dim, second dim consists of components
       of a factorized probability distribution.
-    * diag: scaling diagonal of shape (D,).
 
     Returns:
     * loss: torch float tensor of shape (N,).
     """
-    # \sum^D_i s_{ii} - { (1/2) * \sum^D_i  h_i**2) + (D/2) * log(2\pi) }
-    return torch.sum(diag) - (0.5*torch.sum(torch.pow(h,2),dim=1) + h.size(1)*0.5*torch.log(torch.tensor(2*np.pi)))
+    # (-1/2) * { h_i**2 + log(2\pi) }
+    LOG2PI = torch.log(torch.tensor(2*np.pi))
+    return -0.5 * (torch.pow(h,2) + LOG2PI)
 
-def logistic_nice_loglkhd(h, diag):
+def logistic_nice_loglkhd(h):
     """
     Definition of log-likelihood function with a Logistic prior.
     
     Same arguments/returns as gaussian_nice_loglkhd.
     """
-    # \sum^D_i s_{ii} - { \sum^D_i log(exp(h)+1) + torch.log(exp(-h)+1) }
-    return (torch.sum(diag) - (torch.sum(torch.log1p(torch.exp(h)) + torch.log1p(torch.exp(-h)), dim=1)))
+    # - { log(exp(h)+1) + torch.log(exp(-h)+1) }
+    return -( F.softplus(h, beta=1., threshold=99999.) + F.softplus(-h, beta=1., threshold=99999.) )
 
-# wrap above loss functions in Modules:
+# wrap above loss functions in Modules and incorporate diagonal scaling matrix:
 class GaussianPriorNICELoss(nn.Module):
+    """
+    Args:
+    * h: float tensor of shape (N,D). First dimension is batch dim, second dim consists of components
+      of a factorized probability distribution.
+    * diag: scaling diagonal of shape (D,).
+    """
     def __init__(self, size_average=True):
         super(GaussianPriorNICELoss, self).__init__()
         self.size_average = size_average
 
     def forward(self, fx, diag):
         if self.size_average:
-            return torch.mean(-gaussian_nice_loglkhd(fx, diag))
+            return -torch.mean(gaussian_nice_loglkhd(fx) + torch.sum(diag))
         else:
-            return torch.sum(-gaussian_nice_loglkhd(fx, diag))
+            return -torch.sum(gaussian_nice_loglkhd(fx) + torch.sum(diag))
 
 class LogisticPriorNICELoss(nn.Module):
+    """
+    Args:
+    * h: float tensor of shape (N,D). First dimension is batch dim, second dim consists of components
+      of a factorized probability distribution.
+    * diag: scaling diagonal of shape (D,).
+    """
     def __init__(self, size_average=True):
         super(LogisticPriorNICELoss, self).__init__()
         self.size_average = size_average
 
     def forward(self, fx, diag):
         if self.size_average:
-            return torch.mean(-logistic_nice_loglkhd(fx, diag))
+            return -torch.mean(logistic_nice_loglkhd(fx) + torch.sum(diag))
         else:
-            return torch.sum(-logistic_nice_loglkhd(fx, diag))
+            return -torch.sum(logistic_nice_loglkhd(fx) + torch.sum(diag))
